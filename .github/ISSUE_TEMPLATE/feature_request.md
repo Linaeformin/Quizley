7404663a30cd38fcb92399f4658fa29f2f1e8087
Linaeformin <minsa030407@gmail.com>
2025-10-04 19:33:48 +0900

    docs(github): 프로젝트 기초 세팅

---
+++ .github/ISSUE_TEMPLATE/feature_request.md
@@ -0,0 +1,22 @@
+---
+name: "FEATURE"
+about: "새 기능을 제안할 때 사용"
+title: "[FEAT] "
+labels: ["enhancement"]
+---
+
+## 💡Description
+
+>
+
+---
+
+## 📌 할 일 목록
+
+- [ ] 코드 작성
+- [ ] Postman 확인
+- [ ] API 명세서 작성
+
+---
+
+## 🖼️ 참고 피그마 이미지
---
+++ src/main/java/com/example/quizley/QuizleyApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizleyApplication.class, args);
+	}
+
+}
